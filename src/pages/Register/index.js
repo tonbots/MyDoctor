ad20f6d784c5bcee1aca538a4142a677249353eb
tonbots <kartono.saleh@gmail.com>
2021-11-23 16:43:28 +0700

    Create useForm for integration

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,23 +1,58 @@
 import React from 'react';
-import { StyleSheet,View } from 'react-native';
+import { ScrollView, StyleSheet,View } from 'react-native';
 import { Button, Gap, Header, Input } from '../../components';
-import { colors } from '../../utils';
+import { colors, useForm } from '../../utils';
 
 const Register = ({navigation}) => {
+    // const [fullname, setFullName] = useState('');
+    // const [profission, setProfission] = useState('');
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
+
+    const [form, setForm] = useForm({
+        fullName: '',
+        profission: '',
+        email: '',
+        password: '',
+    });
+
+    const onContinue = () => {
+        console.log(form)
+        // navigation.navigate('UploadPhoto')
+    }
     return (
         <View style={styles.page}>
             <Header title="Daftar Akun" onPress={()=> navigation.goBack()} />
-            <View style={styles.content}>
-                <Input label="Full Name" />
-                <Gap height={24} />
-                <Input label="Pekerjaan" />
-                <Gap height={24} />
-                <Input label="Email Address" />
-                <Gap height={24} />
-                <Input label="Password" />
-                <Gap height={40} />
-                <Button title="Continue" onPress={()=> navigation.navigate('UploadPhoto')} />
-            </View>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={styles.content}>
+                    <Input 
+                        label="Full Name" 
+                        value={form.fullName} 
+                        onChangeText={value=> setForm('fullName',value)} 
+                    />
+                    <Gap height={24} />
+                    <Input 
+                        label="Pekerjaan" 
+                        value={form.profission} 
+                        onChangeText={value=> setForm('profission',value)} 
+                    />
+                    <Gap height={24} />
+                    <Input 
+                        label="Email Address" 
+                        value={form.email} 
+                        onChangeText={value=> setForm('email',value)} 
+                    />
+                    <Gap height={24} />
+                    <Input 
+                        label="Password" 
+                        secureTextEntry 
+                        value={form.password} 
+                        onChangeText={value=> setForm('password',value)} 
+                    />
+                    <Gap height={40} />
+                    <Button title="Continue" onPress={onContinue} />
+                </View>
+            </ScrollView>
         </View>
     );
 };
